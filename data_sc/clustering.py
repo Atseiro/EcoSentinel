b3c728b5688166240b1939c0f80563b300318551
BITOUMBOU BIBOKA <tiger@Host-001.lan>
2025-01-21 23:25:27 +0100

    a jour

--- data_sc/clustering.py
+++ data_sc/clustering.py
@@ -1,8 +1,28 @@
 # IMPORTATION DES LIBRAIRIES
+import pyodbc
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+
+import os
+
+# Documentation technique du  composant
+'''
+Pour le model de Clustering, nous avons du créer nos propre données, les filtrer et enfin utiliser le KMeans comme model non-supervisé de
+la librairie scikit-learn. les Cluster son faient en fonction du nombre de projet. Il y'aura donc autant de Cluster que de project.
+Ainsi donc les utilisateur seront regroupé en fonction du projet commun.
+extrait des données utilisées data.json :
+"user_id": 1,
+        "username": "ecogreen123",     
+        "goal_id": 26,
+        "goal_description": "Réduire la consommation d'objets matériels en privilégiant des choix de qualité et durables.",
+        "progress": "60%",
+        "deadline": "2025-12-31"
+        },
+'''
+
 
 # CHARGEMENT DU FICHIER
 data = json.loads(open('data.json').read())
@@ -28,14 +48,14 @@ X['cluster'] = y_kmeans
 X_array = X.values
 
 
-plt.figure(figsize=(8, 6))
+##plt.figure(figsize=(8, 6))
 for cluster in range(unique_goal_ids):
-    plt.scatter(
-        # Coordonées de points de X dans le cluster
+   plt.scatter(
+      #   Coordonées de points de X dans le cluster
         X_array[y_kmeans == cluster, 0],  
-        # Coordonées de points de Y dans le cluster
+         #Coordonées de points de Y dans le cluster
         X_array[y_kmeans == cluster, 1], 
-        # Étiquette pour chaque cluster
+         #Étiquette pour chaque cluster
         label=f'Cluster {cluster + 1}'  
     )
 
